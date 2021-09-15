509e755db6599259a35cdb06f86da682db0d453a
tech-guru <vinodk9@gmail.com>
2021-09-14 23:01:15 -0700

    chore: update action types

--- actions/recipes.js
+++ actions/recipes.js
@@ -1,16 +1,38 @@
+//
+//
+//import { ADD_RECIPE, FETCH_RECIPES, SET_RECIPES } from '../constants/actionTypes';
+//
+////action creator? -> it's hard to remember the propertirs that need to be passed in the action object
+//
+//const recipesReducer = (recipes = [], action) => {
+//  switch (action.type) {
+//    case ADD_RECIPE:
+//      return recipes.concat({ name: action.name });
+//
+//    case SET_RECIPES:
+//      return action.recipes;
+//  }
+//
+//  return recipes;
+//};
+//
+//export default recipesReducer;
 
 
-import { ADD_RECIPE, FETCH_RECIPES, SET_RECIPES } from '../constants/actionTypes';
+//import { ADD_RECIPE, SET_RECIPES } from '../constants/actionTypes';
 
 //action creator? -> it's hard to remember the propertirs that need to be passed in the action object
-export const addRecipe = (name) => ({
-  type: ADD_RECIPE, name
-});
 
-export const fetchRecipes = () => ({
-  type: FETCH_RECIPES
-});
+const recipesReducer = (recipes = [], action) => {
+  switch (action.type) {
+    case ADD_RECIPE:
+      return recipes.concat({ name: action.name });
 
-export const setRecipes = (recipes) => ({
-  type: SET_RECIPES, recipes
-});
+    case SET_RECIPES:
+      return action.recipes;
+  }
+
+  return recipes;
+};
+
+export default recipesReducer;
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import store from './store';
-import { addRecipe } from './actions/recipes';
+//import { addRecipe } from './actions/recipes';
 import { addIngredient } from './actions/ingredients';
 
 
-store.dispatch(addRecipe('Pancakes'));
+//store.dispatch(addRecipe('Pancakes'));
 
 store.dispatch(addIngredient('Pancakes', 'Egg', 3));
 
